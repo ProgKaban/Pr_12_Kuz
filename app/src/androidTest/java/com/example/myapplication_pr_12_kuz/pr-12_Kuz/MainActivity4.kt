43ef25fdd84e6198bdae6a890603cf9f91c4d374
ProgKaban <aalshsshmnm@bk.ru>
2022-10-05 15:06:49 +0700

    Kuzminyh

---
+++ app/src/androidTest/java/com/example/myapplication_pr_12_kuz/pr-12_Kuz/MainActivity4.kt
@@ -0,0 +1,12 @@
+package com.`Pr-12 v1`.`pr-12_Kuz`
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pr12antuh.R
+
+class MainActivity4 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main4)
+    }
+}
